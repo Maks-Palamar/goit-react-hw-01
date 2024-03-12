3b1a4e07b0cf7ad6b37dfc83e755a39c0a55a8b5
Maksym Palamarchuk <97300407+Maks-Palamar@users.noreply.github.com>
2024-03-12 04:16:44 +0200

    2 tasks without css modules

---
+++ react-hw-01/src/components/Profile.jsx
@@ -0,0 +1,36 @@
+const Profile = props => {
+
+  return (
+    <>
+      <div>
+        <div>
+            <img
+                src={props.image}
+                alt="User avata"
+            />
+            <p>{props.name}</p>
+            <p>@{props.tag}</p>
+            <p>{props.location}</p>
+        </div>
+
+        <ul>
+            <li>
+                <span>Followers</span>
+                <span>{props.stats.followers}</span>
+            </li>
+            <li>
+                <span>Views</span>
+                <span>{props.stats.views}</span>
+            </li>
+            <li>
+                <span>Likes</span>
+                <span>{props.stats.likes}</span>
+            </li>
+        </ul>
+    </div>
+
+    </>
+  )
+}
+
+export default Profile
